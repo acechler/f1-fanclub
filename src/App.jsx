13ad8514295db49ebde630d29168008d1ff53e18
Aaron Echler <acechler@gmail.com>
2024-07-04 17:39:09 -0400

    Implemented API with RacerComponent and RacerDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import RacerDashboard
+ from "./components/RacerDashboard";
 const HomePage = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
@@ -19,7 +20,7 @@ function App() {
         <div className="flex-1 p-6 lg:p-8 bg-gray-100">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/racers" element={<div>F1 Racers</div>} />
+            <Route path="/racers" element={<div>F1 Racers <RacerDashboard/> </div>} />
             <Route path="/about" element={<div>About</div>} />
             <Route path="/news" element={<div>News</div>} />
           </Routes>
---
+++ src/components/RacerComponent.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const RacerComponent = ({ headshot_url, full_name, driver_number }) => {
+  return (
+    <div className="transform transition duration-300 hover:scale-105 rounded-lg shadow-lg w-56 hover:shadow-xl bg-white">
+      <div className="bg-gradient-to-br from-slate-100 via-slate-200 to-slate-400 p-4 rounded-t-lg">
+        <img
+          src={headshot_url}
+          alt={full_name}
+          className="h-32 w-32 object-cover rounded-full mx-auto"
+          loading="lazy" // Add lazy loading for better performance
+          style={{ maxWidth: "100%", height: "auto" }} // Ensure the image maintains its aspect ratio
+        />
+      </div>
+      <div className="p-4">
+        <h2 className="font-semibold text-center text-lg">{full_name}</h2>
+        <h2 className="font-semibold text-center text-lg">{driver_number}</h2>
+        <button
+          className="bg-blue-500 w-full cursor-pointer text-white px-4 py-2 mt-4 rounded-md transition duration-150 hover:bg-blue-700"
+          type="button"
+        >
+          View Profile
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RacerComponent;
