a0daa30c0080ef1d3dca9b8f8d0d22de5ffb543c
Aaron Echler <acechler@gmail.com>
2024-07-08 14:49:29 -0400

    Added Hero to the homepage

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../Hero';
+
+const HomePage = () => {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+        <Hero/>
+      </div>
+    );
+  }
+
+  export default HomePage;
