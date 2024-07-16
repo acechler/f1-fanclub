aeae5fe8484bc94b793f894b568d5abc6d608996
Aaron Echler <acechler@gmail.com>
2024-07-16 17:54:50 -0400

    Add a space in the hero title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,9 @@ const Hero = () => {
                     <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         {/* Hero Image */}
                     <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={heroImg} alt="F1 Race" />
-                        <div className="sm:text-center lg:text-left">
+                        <div className="sm:text-center lg:text-left lg:py-6">
                             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                <span className="block xl:inline">Welcome to</span>
+                                <span className="block xl:inline">Welcome to </span>
                                 <span className="block text-indigo-600 xl:inline">Formula 1 Fanclub</span>
                             </h1>
                             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">The race does not stop until its finished.</p>
