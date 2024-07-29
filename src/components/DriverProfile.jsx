a196c4a288082f681840c73434a9eaa429e06c24
Aaron Echler <acechler@gmail.com>
2024-07-29 19:52:23 -0400

    Add some images into racers page

--- src/components/DriverProfile.jsx
+++ src/components/DriverProfile.jsx
@@ -1,42 +1,33 @@
 import React, { useState } from "react";
-
 import PropTypes from "prop-types";
+import RacerMap from "../components/Utilities/RacerMap"; // Adjusted the import path
 
 const DriverProfile = ({ driver }) => {
+  const racerMap = new RacerMap();
+  const headshotUrl = racerMap.getImageSrc(driver.last_name);// || driver.headshot_url;
 
   return (
     <div className="flow-root">
-
-
-        <img
-          src={driver.headshot_url}
-          alt={driver.full_name}
-          className="object-cover rounded-full mx-auto"
-          loading="lazy" // Add lazy loading for better performance
-          style={{ maxWidth: "100%", height: "auto" }} // Ensure the image maintains its aspect ratio
-        />
-
+      <img
+        src={headshotUrl}
+        alt={driver.full_name}
+        className="object-cover rounded-full mx-auto"
+        loading="lazy" // Add lazy loading for better performance
+        style={{ maxWidth: "100%", height: "auto" }} // Ensure the image maintains its aspect ratio
+      />
       <dl className="-my-3 divide-y divide-gray-100 text-sm">
-
-
         <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
           <dt className="font-medium text-gray-900">Name</dt>
           <dd className="text-gray-700 sm:col-span-2">{driver.full_name}</dd>
         </div>
-
-
         <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-          <dt className="font-medium text-gray-900">Team </dt>
+          <dt className="font-medium text-gray-900">Team</dt>
           <dd className="text-gray-700 sm:col-span-2">{driver.team_name}</dd>
         </div>
-
         <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-          <dt className="font-medium text-gray-900">Number </dt>
+          <dt className="font-medium text-gray-900">Number</dt>
           <dd className="text-gray-700 sm:col-span-2">{driver.driver_number}</dd>
         </div>
-
-  
-
         <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
           <dt className="font-medium text-gray-900">Bio</dt>
           <dd className="text-gray-700 sm:col-span-2">
@@ -54,10 +45,10 @@ const DriverProfile = ({ driver }) => {
 DriverProfile.propTypes = {
   driver: PropTypes.shape({
     full_name: PropTypes.string.isRequired,
+    last_name: PropTypes.string.isRequired,
     driver_number: PropTypes.number.isRequired,
-    team_name : PropTypes.string.IsRequired,
-    headshot_url : PropTypes.string,
-    // Add more prop types for other driver details
+    team_name: PropTypes.string.isRequired,
+    headshot_url: PropTypes.string,
   }).isRequired,
 };
 
--- src/components/Utilities/RacerMap.js
+++ src/components/Utilities/RacerMap.js
@@ -1,39 +1,33 @@
+//todo convert to jpg and to firstname
 class RacerMap {
     constructor() {
-      // Initialize the dictionary with some racers and their image sources
-        const f1Racers = new Map([
-        ["Verstappen", ""],
-        ["Perez", ""],
-        ["Hamilton", ""],
-        ["Russell", ""],
-        ["Leclerc", ""],
-        ["Sainz", ""],
-        ["Norris", ""],
-        ["Piastri", ""],
-        ["Alonso", ""],
-        ["Stroll", ""],
-        ["Ocon", ""],
-        ["Gasly", ""],
-        ["Bottas", ""],
-        ["Zhou", ""],
-        ["Magnussen", ""],
-        ["Hulkenberg", ""],
-        ["Albon", ""],
-        ["Sargeant", ""],
-        ["Tsunoda", ""],
-        ["Ricciardo", ""]
+        this.f1Racers = new Map([
+            ["Verstappen", "src/assets/images/racers/max.jpg"],
+            ["Perez", "src/assets/images/racers/sergio.jpg"],
+            ["Hamilton", "src/assets/images/racers/lewis.jpg"],
+            ["Russell", "src/assets/images/racers/george.jpg"],
+            ["Leclerc", "src/assets/images/racers/charles.jpg"],
+            ["Sainz", "src/assets/images/racers/carlos.jpg"],
+            ["Norris", "src/assets/images/racers/lando.jpg"],
+            ["Piastri", "src/assets/images/racers/oscar.jpg"],
+            ["Alonso", "src/assets/images/racers/fernando.jpg"],
+            ["Stroll", "src/assets/images/racers/stroll.png"],
+            ["Ocon", "src/assets/images/racers/ocon.png"],
+            ["Gasly", "src/assets/images/racers/gasly.png"],
+            ["Bottas", "src/assets/images/racers/bottas.png"],
+            ["Zhou", "src/assets/images/racers/zhou.png"],
+            ["Magnussen", "src/assets/images/racers/magnussen.png"],
+            ["Hulkenberg", "src/assets/images/racers/hulkenberg.png"],
+            ["Albon", "src/assets/images/racers/albon.png"],
+            ["Sargeant", "src/assets/images/racers/sargeant.png"],
+            ["Tsunoda", "src/assets/images/racers/tsunoda.png"],
+            ["Ricciardo", "src/assets/images/racers/ricciardo.png"]
         ]);
-    };
-
-
-
-    // Remove the extra closing brace
-  
-    // Method to get the image source by racer's full name
-    getImageSrc(racerFullName) {
-      return this.f1Racers[racerFullName] || null;
     }
-  
 
+    getImageSrc(racerLastName) {
+        return this.f1Racers.get(racerLastName) || null;
+    }
 }
-  
+
+export default RacerMap;
